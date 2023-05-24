0bf47e246e08f4713f92e60bbd09addf8f6a1e6f
Yoninia Karmely <yoninaweinberg28@gmail.com>
2023-05-23 21:46:57 -0400

    complete

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ const messageConfig = {
       "Loki"
   ]
 };
+const printCard = function () {
+  console.log(this.frontContent);
+  console.log(this.insideContent);
+  // Wow! Elegant! And notice the arrow function's `this` is the same `this`
+  // that printCard has; specifically, the `thisArg` that was passed to it
+  this.signatories.forEach((signatory) =>
+    console.log(`${this.closing[signatory]}, ${signatory}`)
+  );
+};
+
+printCard.call(messageConfig);
